568d0a37487b2ed9e1275ae213ae0928853adb27
insanansharyrasul <insanansryrasul21@gmail.com>
2025-01-06 23:27:04 +0700

    Now the app is connected to the github api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:github_api/src/github_login.dart';
+import 'github_oatuh_credential.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(home: MyHomePage(title: 'Github Client'));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(
+        builder: (context, httpClient) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+              elevation: 2,
+            ),
+            body: Center(child: Text('You are logged into Github!'),)
+          );
+        },
+        githubClientId: githubClientId,
+        githubClientSecret: githubClientSecret,
+        githubScopes: githubScopes);
   }
 }
