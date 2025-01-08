3bb27492e20169e1e45f6431dfdb37896ea2806d
insanansharyrasul <insanansryrasul21@gmail.com>
2025-01-08 12:34:07 +0700

    fix - removed unnecessary import

--- lib/src/github_summary.dart
+++ lib/src/github_summary.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:github/github.dart';
 import 'package:fluttericon/octicons_icons.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class GithubSummary extends StatefulWidget {
